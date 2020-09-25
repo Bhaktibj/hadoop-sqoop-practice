79f2eb277e6f70faefd9b98f202f4de2037a3db1
bhaktibj <bhaktibj402@gmail.com>
2020-09-25 18:27:38 +0530

    added generated java files

---
+++ generated-java-files/users.java
@@ -0,0 +1,455 @@
+// ORM class for table 'users'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Wed Aug 26 12:38:40 IST 2020
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import com.cloudera.sqoop.lib.JdbcWritableBridge;
+import com.cloudera.sqoop.lib.DelimiterSet;
+import com.cloudera.sqoop.lib.FieldFormatter;
+import com.cloudera.sqoop.lib.RecordParser;
+import com.cloudera.sqoop.lib.BooleanParser;
+import com.cloudera.sqoop.lib.BlobRef;
+import com.cloudera.sqoop.lib.ClobRef;
+import com.cloudera.sqoop.lib.LargeObjectLoader;
+import com.cloudera.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class users extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  public static interface FieldSetterCommand {    void setField(Object value);  }  protected ResultSet __cur_result_set;
+  private Map<String, FieldSetterCommand> setters = new HashMap<String, FieldSetterCommand>();
+  private void init0() {
+    setters.put("user_id", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        users.this.user_id = (Integer)value;
+      }
+    });
+    setters.put("username", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        users.this.username = (String)value;
+      }
+    });
+    setters.put("password", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        users.this.password = (String)value;
+      }
+    });
+    setters.put("email", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        users.this.email = (String)value;
+      }
+    });
+  }
+  public users() {
+    init0();
+  }
+  private Integer user_id;
+  public Integer get_user_id() {
+    return user_id;
+  }
+  public void set_user_id(Integer user_id) {
+    this.user_id = user_id;
+  }
+  public users with_user_id(Integer user_id) {
+    this.user_id = user_id;
+    return this;
+  }
+  private String username;
+  public String get_username() {
+    return username;
+  }
+  public void set_username(String username) {
+    this.username = username;
+  }
+  public users with_username(String username) {
+    this.username = username;
+    return this;
+  }
+  private String password;
+  public String get_password() {
+    return password;
+  }
+  public void set_password(String password) {
+    this.password = password;
+  }
+  public users with_password(String password) {
+    this.password = password;
+    return this;
+  }
+  private String email;
+  public String get_email() {
+    return email;
+  }
+  public void set_email(String email) {
+    this.email = email;
+  }
+  public users with_email(String email) {
+    this.email = email;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof users)) {
+      return false;
+    }
+    users that = (users) o;
+    boolean equal = true;
+    equal = equal && (this.user_id == null ? that.user_id == null : this.user_id.equals(that.user_id));
+    equal = equal && (this.username == null ? that.username == null : this.username.equals(that.username));
+    equal = equal && (this.password == null ? that.password == null : this.password.equals(that.password));
+    equal = equal && (this.email == null ? that.email == null : this.email.equals(that.email));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof users)) {
+      return false;
+    }
+    users that = (users) o;
+    boolean equal = true;
+    equal = equal && (this.user_id == null ? that.user_id == null : this.user_id.equals(that.user_id));
+    equal = equal && (this.username == null ? that.username == null : this.username.equals(that.username));
+    equal = equal && (this.password == null ? that.password == null : this.password.equals(that.password));
+    equal = equal && (this.email == null ? that.email == null : this.email.equals(that.email));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.user_id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.username = JdbcWritableBridge.readString(2, __dbResults);
+    this.password = JdbcWritableBridge.readString(3, __dbResults);
+    this.email = JdbcWritableBridge.readString(4, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.user_id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.username = JdbcWritableBridge.readString(2, __dbResults);
+    this.password = JdbcWritableBridge.readString(3, __dbResults);
+    this.email = JdbcWritableBridge.readString(4, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(user_id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeString(username, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(password, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(email, 4 + __off, 12, __dbStmt);
+    return 4;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(user_id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeString(username, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(password, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(email, 4 + __off, 12, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.user_id = null;
+    } else {
+    this.user_id = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.username = null;
+    } else {
+    this.username = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.password = null;
+    } else {
+    this.password = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.email = null;
+    } else {
+    this.email = Text.readString(__dataIn);
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.user_id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.user_id);
+    }
+    if (null == this.username) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, username);
+    }
+    if (null == this.password) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, password);
+    }
+    if (null == this.email) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, email);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.user_id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.user_id);
+    }
+    if (null == this.username) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, username);
+    }
+    if (null == this.password) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, password);
+    }
+    if (null == this.email) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, email);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(user_id==null?"null":"" + user_id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(username==null?"null":username, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(password==null?"null":password, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(email==null?"null":email, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(user_id==null?"null":"" + user_id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(username==null?"null":username, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(password==null?"null":password, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(email==null?"null":email, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.user_id = null; } else {
+      this.user_id = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.username = null; } else {
+      this.username = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.password = null; } else {
+      this.password = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.email = null; } else {
+      this.email = __cur_str;
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.user_id = null; } else {
+      this.user_id = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.username = null; } else {
+      this.username = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.password = null; } else {
+      this.password = __cur_str;
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null")) { this.email = null; } else {
+      this.email = __cur_str;
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    users o = (users) super.clone();
+    return o;
+  }
+
+  public void clone0(users o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new HashMap<String, Object>();
+    __sqoop$field_map.put("user_id", this.user_id);
+    __sqoop$field_map.put("username", this.username);
+    __sqoop$field_map.put("password", this.password);
+    __sqoop$field_map.put("email", this.email);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("user_id", this.user_id);
+    __sqoop$field_map.put("username", this.username);
+    __sqoop$field_map.put("password", this.password);
+    __sqoop$field_map.put("email", this.email);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if (!setters.containsKey(__fieldName)) {
+      throw new RuntimeException("No such field:"+__fieldName);
+    }
+    setters.get(__fieldName).setField(__fieldVal);
+  }
+
+}
